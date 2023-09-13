18855ca91218ea3ba2e40a8cdbe64dbb9eb1d400
CsitariA <CsitariA@WINDOWS-JRLGTOD>
2023-09-13 12:51:00 +0200

    Add project files.

---
+++ ProductApi/Dtos.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi
+{
+    public class Dtos
+    {
+        public record ProductDto(Guid Id, string ProductName, int ProductPrice, DateTimeOffset CreateTime);
+        public record CreateProductDto(string ProductName, int ProductPrice);
+
+        public record PullProductDto(string ProductName, int ProductPrice);
+    }
+}
